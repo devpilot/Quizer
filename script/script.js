3d0a37250b9745d5b120f5c17614c2c240e32fc7
Pilot <aet419@gmail.com>
2012-03-22 02:46:27 +0530

    added script.js with timer and starting effect

---
+++ script/script.js
@@ -0,0 +1,44 @@
+// Setup Timeing
+var h = 0,m = 0, s = 10;
+
+/**
+* Add leading 0 to int
+*/
+function pad(num) {
+    var s = num+"";
+    if(s.length < 2) s = "0" + s;
+    return s;
+}
+
+/**
+* Countdown Timer
+*/
+function timer(){
+    if(s == 0){
+        if(m > 0){
+            s = 59;
+            m--;
+        }
+    } else {
+        s--;
+    }
+    if(m == 0){
+        if(h > 0){
+            m = 59;
+            h--;
+        }
+    }
+    if(h == 0 && m == 0 && s == 0){
+        // Timeup, Stop timer
+        clearInterval(timerF);
+    $(".screen").slideDown(1000);
+    }
+    // Setting time in span#time
+    $("#time").text(h+':'+pad(m)+':'+pad(s));
+}
+$(document).ready(function(){
+   $("button#qstart").click(function(){
+           $(".screen").slideUp(1000);
+          timerF = setInterval(timer,1000); 
+       }); 
+});
