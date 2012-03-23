f40a4947b76229856ca37ea5dd239cf6cc650cdd
Pilot <aet419@gmail.com>
2012-03-23 11:02:08 +0530

    button show/hide function, finish button added

--- script/script.js
+++ script/script.js
@@ -60,7 +60,26 @@ function getQuiz(now){
         $.each(data.quiz[now].option, function(i,op){
             //$(".options").append("<li>"+op+"</li>");
         })
+        btnHandler(now);
     });
+}
+
+/**
+ * Show or hide buttons
+ */
+function btnHandler(now){
+    if(now+1 == totalQ){
+        $("button#next-btn").hide();
+        $("button#fin-btn").show();
+    } else {
+        $("button#fin-btn").hide();
+        $("button#next-btn").show();
+    }
+    if(now == 0){
+        $("button#prev-btn").hide();
+    } else {
+        $("button#prev-btn").show();
+    }
 }
 
 $(document).ready(function(){
