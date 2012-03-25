b2ec77ea457af56c72270dd9e9740f41f9285bad
Pilot <aet419@gmail.com>
2012-03-25 13:48:50 +0530

    additional files included

---
+++ README.md
@@ -0,0 +1,4 @@
+###Quizer
+==================
+
+A HTML5 & JQuery based quiz application can run on a browser without a server.
---
+++ quiz.js
@@ -0,0 +1,95 @@
+{"quiz":
+	[
+		{
+			"question":"What does HTML stand for?",
+			"option": {
+					"a":"Home Tool Markup Language",
+					"b":"Hyperlinks and Text Markup Language",
+					"c":"Hyper Text Markup Language"
+			},
+			"ans":"c"
+		},
+		{
+			"question":"Who is making the Web standards?",
+			"option": {
+					"a":"Microsoft",
+					"b":"The World Wide Web Consortium",
+					"c":"Mozilla"
+			},
+			"ans":"b"
+		},
+		{
+			"question":"Choose the correct HTML tag for the largest heading",
+			"option": {
+					"a":"&lt;h1&gt;",
+					"b":"&lt;head&gt;",
+					"c":"&lt;h6&gt;",
+					"d":"&lt;heading&gt;"
+			},
+			"ans":"a"
+		},
+		{
+			"question":"What is the correct HTML tag for inserting a line break?",
+			"option": {
+					"a":"&lt;break /&gt;",
+					"b":"&lt;br /&gt;",
+					"c":"&lt;lb /&gt;"
+			},
+			"ans":"b"
+		},
+		{
+			"question":"What is the preferred way for adding a background color in HTML?",
+			"option": {
+					"a":"&lt;body background=\"yellow\"&gt;",
+					"b":"&lt;body style=\"background-color:yellow\"&gt;",
+					"c":"&lt;background&gt;yellow&lt;/background&gt;"
+			},
+			"ans":"b"
+		},
+		{
+			"question":"Choose the correct HTML tag to make a text bold",
+			"option": {
+					"a":"&lt;bold&gt;",
+					"b":"&lt;b&gt;"
+			},
+			"ans":"b"
+		},
+		{
+			"question":"Choose the correct HTML tag to make a text italic",
+			"option": {
+					"a":"&lt;italic&gt;",
+					"b":"&lt;i&gt;"
+			},
+			"ans":"b"
+		},
+		{
+			"question":"What is the correct HTML for creating a hyperlink?",
+			"option": {
+					"a":"&lt;a name=\"http://www.w3schools.com\"&gt;W3Schools.com&lt;/a&gt;",
+					"b":"&lt;a&gt;http://www.w3schools.com&lt;/a&gt;",
+					"c":"&lt;a href=\"http://www.w3schools.com\"&gt;W3Schools&lt;/a&gt;",
+					"d":"&lt;a url=\"http://www.w3schools.com\"&gt;W3Schools.com&lt;/a&gt;"
+			},
+			"ans":"c"
+		},
+		{
+			"question":"How can you create an e-mail link?",
+			"option": {
+					"a":"&lt;mail href=\"xxx@yyy\"&gt;",
+					"b":"&lt;mail&gt;xxx@yyy&lt;/mail&gt;",
+					"c":"&lt;a href=\"xxx@yyy\"&gt;",
+					"d":"&lt;a href=\"mailto:xxx@yy\"&gt;"
+			},
+			"ans":"d"
+		},
+		{
+			"question":"How can you open a link in a new browser window?",
+			"option": {
+					"a":"&lt;a href=\"url\" target=\"_blank\"&gt;",
+					"b":"&lt;a href=\"url\" target=\"new\"&gt;",
+					"c":"&lt;a href=\"url\" new&gt;"
+			},
+			"ans":"a"
+		}
+	]
+}
